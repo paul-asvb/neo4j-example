2085e7eae1b0bfb8198cc06aed0482509d12f5f5
Paul <paul@paul-mayer.eu>
2020-07-06 10:46:02 +0200

    init local DB

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 var neo4j = require('neo4j-driver');
-var driver = neo4j.driver('bolt://107.22.147.78:33953', neo4j.auth.basic('neo4j', 'thresholds-wines-generators'));
+var driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'neo4j'));
 
-var query = 
-  "MATCH (n) \
-   RETURN ID(n) as id \
-   LIMIT $limit";
+var query =
+    `CREATE (adam:User { name: 'Adam' }),(pernilla:User { name: 'Pernilla' }),(david:User { name: 'David'
+}),
+(adam)-[:FRIEND]->(pernilla),(pernilla)-[:FRIEND]->(david)`;
 
-var params = {"limit": 10};
+var params = { "limit": 10 };
 
 var session = driver.session();
 
 session.run(query, params)
-  .then(function(result) {
-    result.records.forEach(function(record) {
-        console.log(record.get('id'));
+    .then(function (result) {
+        result.records.forEach(function (record) {
+            console.log(record.get('id'));
+        })
     })
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
+var query =
+    `MATCH (n)
+RETURN n`;
+
+var params = { "limit": 10 };
+
+var session = driver.session();
+
+session.run(query, params)
+    .then(function (result) {
+        result.records.forEach(function (record) {
+            console.log(record);
+        })
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+driver.close()
+    .catch(function (error) {
+        console.log(error);
+    }); 
